f6ebe60111e995d2b90f20ce8b9cfc45b2c2d3f2
jarlhelin <jarl.helin@live.fr>
2015-03-23 16:15:48 +0100

    Added full responsive grid with ie compatibility

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
+#STARTER KIT FOR WEB PROJECTS - Version 0.9.1
 =============================
-#STARTER KIT FOR WEB PROJECTS
-=============================
-
 ##OWNER - Jarl HELIN MIAILHE
 =============================
 ![Logotype](http://jarlhelin.fr/thunderbird/jhdwpLogo.png)
 Email: info@jarlhelin.fr
-Website: [Goto Jarl's Website for more info](http://jarlhelin.fr/ "Goto Jarl's Website for more info").
+###Website: [Goto Jarl's Website for more info](http://jarlhelin.fr/ "Goto Jarl's Website for more info").
 =============================
+####LASTEST UPDATE - February 2015
--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,32 @@
+/*--FUNCTIONS.JS--*/
+
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+/** CUSTOM SCRIPTS **/
 $( document ).ready(function() {
-    alert( "Ready to script!" );
+    console.log('jQuery is on!');
 });
+
+
+
 
